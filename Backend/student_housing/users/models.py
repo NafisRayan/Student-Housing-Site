656f400990de25b65da9bdbe356ee635b1e6eabe
AnindaNath <vaskoraninda.nath.01@gmail.com>
2023-11-01 14:46:13 +0600

    register db

--- Backend/student_housing/users/models.py
+++ Backend/student_housing/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class register(models.Model):
+    username=models.CharField(max_length=30)
+    email=models.EmailField()
+    password=models.IntegerField() 
+    nid=models.IntegerField() 
