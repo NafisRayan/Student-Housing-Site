5319e011deed8d1a6b2013e14cfe6a31bdfc1b1f
Nafis Rayan <90589446+NafisRayan@users.noreply.github.com>
2023-11-17 23:38:36 +0600

    2m

--- Backend/student_housing/users/models.py
+++ Backend/student_housing/users/models.py
@@ -8,4 +8,11 @@ class Register(models.Model):
     password=models.CharField(max_length=100) 
     nid=models.IntegerField() 
 
-    
+class DormRooms(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    popularity = models.CharField(max_length=20)
+    type = models.CharField(max_length=50)
+    price = models.CharField(max_length=20)
+    link = models.URLField(max_length=200)
