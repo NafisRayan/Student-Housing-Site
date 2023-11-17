6ce7cd7891f40d395b7be8911cb2cf21ad7946e0
Raahim-Rubaiyat-Teertho <raahim.rubaiyat2021@gmail.com>
2023-11-18 00:06:22 +0600

    models change

--- Backend/student_housing/users/admin.py
+++ Backend/student_housing/users/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Register
+from .models import Register, Comment, DormRoom
 # Register your models here.
 
 class RegisterAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'password', 'nid')
 
 admin.site.register(Register, RegisterAdmin)
+admin.site.register(Comment)
+admin.site.register(DormRoom)
