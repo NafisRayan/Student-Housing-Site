06b40552be146dd0deb84ddc76e8753fa0d14f31
Nafis Rayan <90589446+NafisRayan@users.noreply.github.com>
2023-11-17 23:51:01 +0600

    2a

--- Backend/student_housing/users/admin.py
+++ Backend/student_housing/users/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import Register
+from .models import Register, DormRooms
 # Register your models here.
 
 class RegisterAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'password', 'nid')
 
 admin.site.register(Register, RegisterAdmin)
+
+class DormRoomsAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'title', 'content', 'popularity', 'type', 'price', 'link')
+
+
+
+admin.site.register(DormRooms)
